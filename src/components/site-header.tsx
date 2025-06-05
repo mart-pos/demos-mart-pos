6774d40283524a66233baba1501ebddd35e25f66
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2025-06-05 08:58:07 -0700

    add page inventiories

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -3,7 +3,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export function SiteHeader({ children = "Title" }: { children: string }) {
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+    <header className="flex sticky z-50 top-0 bg-background h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
         <Separator
